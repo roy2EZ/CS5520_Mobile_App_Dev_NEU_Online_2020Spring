82fe12ef68b1b4a61b6eaab468758ec339227022
roychen <chen.rongy@husky.neu.edu>
2020-02-03 13:59:30 -0800

    Update link collector functions and layout

--- app/src/main/java/edu/neu/madcourse/numad20s_rongyichen/LinkCollectorActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad20s_rongyichen/LinkCollectorActivity.java
@@ -16,11 +16,15 @@ import com.google.android.material.snackbar.Snackbar;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * To represent the Link Collector activity
+ * which can save user input websites and go to a website on click
+ */
 public class LinkCollectorActivity extends AppCompatActivity {
-    private List<Website> mUrlList;
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private List<Website> myUrlList;
+    private RecyclerView myRecyclerView;
+    private RecyclerView.Adapter myAdapter;
+    private RecyclerView.LayoutManager myLayoutManager;
 
     private EditText edittextName;
     private EditText edittextUrl;
@@ -30,41 +34,46 @@ public class LinkCollectorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_collector_layout);
 
-        mRecyclerView = findViewById(R.id.my_recycler_view);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
+        myRecyclerView = findViewById(R.id.my_recycler_view);
+        myRecyclerView.setHasFixedSize(true);
+        myLayoutManager = new LinearLayoutManager(this);
 
         FloatingActionButton fab = findViewById(R.id.btn_plus_card);
 
         edittextName = findViewById(R.id.edittext_name);
         edittextUrl = findViewById(R.id.edittext_url);
 
-        this.mUrlList = new LinkedList<>();
+        this.myUrlList = new LinkedList<>();
 
-        mAdapter = new Adapter(mUrlList, new Adapter.OnItemClickListener() {
+        myAdapter = new Adapter(myUrlList, new Adapter.OnItemClickListener() {
             @Override public void onItemClick(Website Website) {
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(Website.getUrl()));
                 startActivity(intent);
             }});
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
+        myRecyclerView.setLayoutManager(myLayoutManager);
+        myRecyclerView.setAdapter(myAdapter);
 
+        /**
+         * To add website after user click on Add button
+         */
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Website Website = new Website(edittextName.getText().toString(), edittextUrl.getText().toString());
-                mUrlList.add(Website);
+                myUrlList.add(Website);
                 Snackbar.make(v, "Website added to list", Snackbar.LENGTH_LONG) .setAction("Undo", undoOnClickListener).show();
-                mAdapter.notifyDataSetChanged();
+                myAdapter.notifyDataSetChanged();
             }
-
         });
     }
 
+    /**
+     * To remove the saved website after user click on undo
+     */
     View.OnClickListener undoOnClickListener = new View.OnClickListener() {
         @Override public void onClick(View view) {
-            mUrlList.remove(mUrlList.size() -1);
-            mAdapter.notifyDataSetChanged();
+            myUrlList.remove(myUrlList.size() -1);
+            myAdapter.notifyDataSetChanged();
             Snackbar.make(view, "Website removed", Snackbar.LENGTH_LONG) .setAction("Action", null).show();
         }
     };
